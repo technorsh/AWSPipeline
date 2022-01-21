8846b86325bfb99bd0e0d92639869eed6a02ddce
Rahul Sharma <37498925+technorsh@users.noreply.github.com>
2022-01-21 12:17:15 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cd /home/ec2-user/server
-curl -fsSL https://rpm.nodesource.com/setup_17.x | sudo bash -
+
+# navigate to app folder
+cd /app
+
+# install node and npm
+curl -fsSL https://rpm.nodesource.com/setup_17.x | sudo -E bash -
 yum -y install nodejs npm
