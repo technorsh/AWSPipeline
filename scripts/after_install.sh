155039b4463d881a28078adc38ef809c484919e0
Rahul Sharma <37498925+technorsh@users.noreply.github.com>
2022-01-21 13:17:19 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,11 +4,11 @@
 cd /app
 
 # install dependencies
-npm install
+sudo npm install
 
 # install create-react-app and react-scripts
 # without react-scripts application cannot be started
-npm install --save create-react-app react-scripts
+sudo npm install --save create-react-app react-scripts
 
 # install pm2 process manager
-npm install pm2 -g
+sudo npm install pm2 -g
